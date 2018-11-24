3c6ebb43361261bd76593da72a318801b072464a
Matheus Castro <goca1377@gmail.com>
2018-11-24 21:41:42 -0200

    criando schema

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import * as express from 'express'
+import * as graphqlHTTP from 'express-graphql'
+
+import schema from './graphql/schema'
 
 class App {
   public express: express.Application
@@ -9,11 +12,10 @@ class App {
   }
 
   private middleware(): void {
-    this.express.use('/hello', (req, res, next) => {
-      res.send({
-        hello: 'Hello World!'
-      })
-    })
+    this.express.use('/graphql', graphqlHTTP({
+      schema: schema,
+      graphiql: true
+    }))
   }
 }
 
